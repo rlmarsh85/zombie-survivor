7baf87817ac6c862b4a2e96eeafaccb34d7b6ac9
rlmarsh85 <rlmarsh85@gmail.com>
2025-09-10 14:21:52 +0200

    Fixing player animations
    player should now animate correctly for pistol

--- player.gd
+++ player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 signal player_died
 
+enum State { IDLE, WALKING }
+
+
 @export var bullet_scene: PackedScene
 @export var muzzle_scene: PackedScene
 @export var speed = 600
@@ -10,13 +13,17 @@ signal player_died
 @onready var bullet_spawn_point = $BulletSpawn
 @onready var muzzle_flash_spawn_point = $MuzzleFlashSpawn
 @onready var cooldown_timer = $CooldownTimer
+@onready var animator = $AnimatedSprite2D
 
 @export var is_dead = true
+var current_state = State.IDLE
+var is_walking = false
+var is_shooting = false
+
 var screen_size
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-
 	
 func _physics_process(delta):
 	look_at(get_global_mouse_position())
@@ -33,14 +40,16 @@ func set_direction(delta):
 		return
 
 	var direction = Input.get_vector("left", "right", "up", "down")
-
-	if direction != Vector2.ZERO:
+	is_walking = direction != Vector2.ZERO
+	if is_walking:
 		velocity = direction * speed
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.play()
+		current_state = State.WALKING
+		
 	else:
+		current_state = State.IDLE
 		velocity = Vector2.ZERO
-		$AnimatedSprite2D.stop()
+		
+	handle_animation()
 
 func handle_shoot(event) -> void:
 	if(!cooldown_timer.is_stopped() || is_dead):
@@ -55,7 +64,20 @@ func handle_shoot(event) -> void:
 	muzzle_flash.global_position = muzzle_flash_spawn_point.global_position
 	muzzle_flash.rotation = self.rotation
 	
+	is_shooting = true
+
 	cooldown_timer.start()
+	
+func handle_animation():
+	if is_shooting:
+		animator.animation = "shoot_pistol"
+	else:
+		if is_walking:
+			animator.animation = "walk_pistol"
+		else:
+			animator.animation = "idle_pistol"
+	
+	animator.play()
 	
 func gets_eaten() -> void:
 	$CollisionShape2D.set_deferred("disabled", true)
@@ -66,3 +88,9 @@ func gets_eaten() -> void:
 func resurrect() -> void:
 	is_dead = false
 	$CollisionShape2D.set_deferred("disabled", false)
+	
+	
+func _on_animated_sprite_2d_animation_finished() -> void:
+	
+	if animator.animation == "shoot_pistol":
+		is_shooting = false
